a34d079d59cef4507ae44012893f342690daabc9
mitchellgsides <mitchellgsides@gmail.com>
2019-02-26 17:42:56 -0600

    updates

--- docs/mgs-portfolio.jsx
+++ docs/mgs-portfolio.jsx
@@ -1,6 +1,29 @@
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
 
-navBarToggle.addEventListener('click', function () {
-  mainNav.classList.toggle('active');
-});
+
+$('.fa-bars').on('click', function(event) {
+  event.preventDefault();
+  $('.navbar').css('display', 'none');
+})
+
+$(function() {
+  $(".fa-dog").on('click', function() {
+    $("html, body").animate({ scrollTop: 0 }, 1000); }); });
+
+function projScroll() {
+  $('#projects').on('click', function(event) {
+    event.preventDefault();
+    let position = $('#proj-anchor').offset().top;
+    $('html, body').animate(
+      {scrollTop: position}, 1000); }
+)}
+
+function aboutScroll() {
+  $('#about').on('click', function(event) {
+    event.preventDefault();
+    $('html, body').animate(
+      {scrollTop: $('#about-anchor').offset().top}, 1000); }
+    )
+  };
+  
+  $(aboutScroll);
+  $(projScroll);
